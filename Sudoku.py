486c6c0c98cde54a0e9a113ba760d488ea5b7716
Fidanrle <fidan.nonnnn5@gmail.com>
2020-07-30 01:36:12 +0400

    updated

--- Sudoku.py
+++ Sudoku.py
@@ -1,65 +1,65 @@
 # Sudoku Solver
 
+
 class Sudoku:
     def __init__(self, val, row, column):
         self.val = val
         self.row = row
         self.cl = column
-        self.r = self.row // 3 * 3  # first row of square room
-        self.c = self.cl // 3 * 3   # first column of square room
-        self.room = []              # square room
-        self.cell = []              # probability numbers
+        self.r = row // 3 * 3  # first row of square room
+        self.c = column // 3 * 3   # first column of square room
 
-    def set_cell(self):             # get probability numbers
-        self.set_room()
-        if self.val == 0:
-            s_ = S_row[self.row] + S_cl[self.cl] + self.room
-            rs = [x for x in range(1, 10) if x not in s_]
+    @property
+    def cell(self):             # get probability numbers for current empty cell
+        s_ = S_row[self.row] + S_cl[self.cl] + self.room
+        rs = [x for x in range(1, 10) if x not in s_]
+        return rs
 
-            self.cell = rs
-            if len(rs) == 1:
-                self.val = rs[0]
+    def check_cell(self):       # check if the cell contains only 1 number
+        if len(self.cell) == 1:
+            self.val = self.cell[0]
+            self.set_list_values()
+            del self
+
+    def check_room(self):            # check probability numbers of all the room members' cell
+        for v in self.cell:          # check if a number in the current cell that does not exist in any member cell
+            if room_cells[str(self.r) + str(self.c)].count(v) == 1:
+                self.val = v
                 self.set_list_values()
-                self.cell = []
+                del self
 
-    def room_cell(self):             # check probability numbers of cells of all the room members
-        if self.val == 0:
-            for v in self.cell:      # check if a number in the current cell that does not exist in any member cell
-                if room_cells[str(self.r) + str(self.c)].count(v) == 1:
-                    self.val = v
-                    self.set_list_values()
-                    self.cell = []
-
-    def room_row_cl(self):
-        if self.val == 0:
-            for v in self.cell:     # check if a number in the current cell that does not exist in any row cell
-                if room_rows[self.row].count(v) == 1:
-                    # print(self.row,self.cl,'room_row_cl',v,self.val)
-                    self.val = v
-                    self.set_list_values()
-                    self.cell = []
+    def check_row_clmn(self):
+        for v in self.cell:       # check if a number in the current cell that does not exist in any row cell
+            if room_rows[self.row].count(v) == 1:
+                # print(self.row,self.cl,'room_row_cl',v,self.val)
+                self.val = v
+                self.set_list_values()
+                del self
                                     # check if a number in the current cell that does not exist in any column cell
-                if room_cl[self.cl].count(v) == 1:
-                    # print(self.row,self.cl,'room_row_cl',v,self.val)
-                    self.val = v
-                    self.set_list_values()
-                    self.cell = []
+            if room_cl[self.cl].count(v) == 1:
+                # print(self.row,self.cl,'room_row_cl',v,self.val)
+                self.val = v
+                self.set_list_values()
+                del self
 
     def set_list_values(self):     # change original row and column lists with current value
         S_cl[self.cl][self.row] = self.val
         S_row[self.row][self.cl] = self.val
 
-    def set_room(self):            # create room
+    @property
+    def room(self):                 # create room
+        room_members = []
         for i in range(self.r, self.r + 3):
             for j in range(self.c, self.c + 3):
-                                   # get number of members if it is not equal to 0
+                # get number of members if it is not equal to 0
                 if S_row[i][j] != 0:
-                    self.room.append(S_row[i][j])
+                    room_members.append(S_row[i][j])
+        return room_members
 
 
 def check_1():
     for key in Sud.keys():
-        Sud[key].set_cell()       # look through rooms, rows and columns to check if probability is only 1
+        Sud[key].check_cell()      # look through rooms, rows and columns to check if probability is only 1
 
 
 def create_room_cell():           # create cells of all rooms
@@ -88,8 +88,9 @@ def check_2():                      # look through cells of rooms, rows and colu
     create_room_cell()
     create_row_cl_cell()
     for key in Sud.keys():
-        Sud[key].room_cell()
-        Sud[key].room_row_cl()
+        Sud[key].check_room()
+        Sud[key].check_row_clmn()
+
 
 def get_Sudoku():                   # store unsolved sudoku
     global S_cl
@@ -112,6 +113,7 @@ def check(n):
             check_1()
         check_2()
 
+
 S_row = []
 Sud = {}
 neighbor = {}
@@ -127,11 +129,11 @@ get_Sudoku()
 pow = input('Enter easy, medium, hard')
 
 if pow == 'easy':
-    check(2)
+    check(1)
 elif pow == 'medium':
     check(4)
 elif pow == 'hard':
-    check(10)
+    check(8)
 
 print(*S_row, sep='\n')
 input()
